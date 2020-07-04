3d059bad3e7703328999eb3a82e2603f5ab7a898
adrifcastr <adrifcastr@gmail.com>
2020-07-04 15:17:16 +0200

    fix stupidity

--- Util.js
+++ Util.js
@@ -75,7 +75,7 @@ class Util {
         return embed;
     }
 
-    static InitStatus() {
+    static async InitStatus() {
         let guilds = process.janet.shard ? await process.janet.shard.fetchClientValues('guilds.cache').catch(ex => console.log(ex)) : [process.janet.guilds.cache.size];
         if (guilds) guilds = [].concat.apply([], guilds);
         process.janet.user.setActivity(`${guilds.length} neighborhoods | -help`, { type: 'WATCHING' }); 
